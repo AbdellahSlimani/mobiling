a4d70091661cea615bb1eb1158b45a6cc36a60e8
Abdellah Slimani <slimaniabdellah.as@gmail.com>
2023-08-27 22:44:49 +0100

    Import footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import About from "./components/About";
+import Agreement from "./components/Agreement";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-red-500">Hello world</h1>
+    <div className="App relative">
+      <Navbar />
+      <Hero />
+      <Agreement />
+      <About />
+      <Footer />
     </div>
   );
 }
